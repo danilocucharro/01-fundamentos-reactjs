cc65dce602e52b8b215095711d08a986a82c0ed3
danilocucharro <danicucharro@outlook.com>
2024-02-02 15:28:04 -0300

    finalizado a sidebar

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,8 @@
 import { Post } from './Post'
 import { Header } from './components/Header'
+import { Sidebar } from './components/Sidebar'
+
+import styles from './App.module.css';
 
 import './global.css'
 
@@ -7,14 +10,20 @@ export function App() {
   return (
     <>
       <Header />
-      <Post 
+
+      <div className={styles.wrapper}>
+        <Sidebar />
+        <main>
+        <Post 
         author="Danilo Cucharro" 
         content="Lorem ipsum, dolor sit amet consectetur adipisicing elit. Iure, nam. Assumenda id commodi beatae. Doloremque doloribus explicabo, nostrum iste quasi ad reiciendis, repellat libero mollitia eaque dolore reprehenderit quae maiores?"
-      />
-      <Post 
-        author="Robson Souza"
-        content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestiae quae id perspiciatis corrupti voluptas, eligendi sunt quo consequuntur modi. Modi repellat aut cum veritatis ab laboriosam, doloremque harum earum ad?"
-      />
+        />
+        <Post 
+          author="Robson Souza"
+          content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestiae quae id perspiciatis corrupti voluptas, eligendi sunt quo consequuntur modi. Modi repellat aut cum veritatis ab laboriosam, doloremque harum earum ad?"
+        />
+        </main>
+      </div>
     </>
   )
 }
---
+++ src/components/Sidebar.jsx
@@ -0,0 +1,28 @@
+import { PencilLine } from '@phosphor-icons/react'
+
+import styles from './Sidebar.module.css'
+
+export function Sidebar(){
+  return(
+    <aside className={styles.sidebar}>
+      <img 
+        className={styles.cover}
+        src="https://images.unsplash.com/photo-1560415755-bd80d06eda60?q=40&w=500&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+      />
+
+      <div className={styles.profile}>
+        <img className={styles.avatar} src="https://github.com/danilocucharro.png" />
+
+        <strong>Danilo Cucharro</strong>
+        <span>Web Developer</span>
+      </div>
+
+      <footer>
+        <a href="#">
+          <PencilLine size={20}/>
+          Editar seu Perfil
+        </a>
+      </footer>
+    </aside>
+  );
+}
