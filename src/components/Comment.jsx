18156e560799e422d4cba59fbac2d9f7b7fd3cf8
danilocucharro <danicucharro@outlook.com>
2024-02-08 10:36:25 -0300

    Criado uma nova função que deleta um comentario

--- src/components/Comment.jsx
+++ src/components/Comment.jsx
@@ -2,7 +2,11 @@ import { Avatar } from './Avatar'
 import { ThumbsUp, Trash } from '@phosphor-icons/react';
 import styles from './Comment.module.css'
 
-export function Comment({ content }) {
+export function Comment({ content, onDeleteComment}) {
+  function handleDeleteComment () {
+    onDeleteComment(content);
+  }
+
   return(
     <div className={styles.comment}>
       <Avatar hasBorder={false} src="https://github.com/danilocucharro.png" />
@@ -15,7 +19,7 @@ export function Comment({ content }) {
               <time title="03 de fevereiro às 08:28" dateTime="2024-02-03 08:28">Cerca de 1h atrás</time>
             </div>
 
-            <button title="Deletar Comentário">
+            <button onClick={handleDeleteComment} title="Deletar Comentário">
               <Trash size={24} />
             </button>
           </header>
--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -33,6 +33,14 @@ export function Post({ author, publishedAt, content }) {
     setNewCommentText(event.target.value)
   }
 
+  function deleteComment(commentToDelete) {
+    const commentsWithoutDeletedOne = comments.filter(comment =>{
+      return comment != commentToDelete
+    })
+
+    setComments(commentsWithoutDeletedOne);
+  }
+
   return(
     <article className={styles.post}>
       <header>
@@ -50,9 +58,9 @@ export function Post({ author, publishedAt, content }) {
       <div className={styles.content}>
         {content.map(line =>{
           if (line.type === 'paragraph') {
-            return <p>{line.content}</p>
+            return <p key={line.content}>{line.content}</p>
           } else if (line.type === 'link') {
-            return <p><a href="">{line.content}</a></p>
+            return <p key={line.content}><a href="">{line.content}</a></p>
           }
         })}
       </div>
@@ -73,7 +81,13 @@ export function Post({ author, publishedAt, content }) {
 
       <div className={styles.commentList}>
         {comments.map(comment => {
-          return <Comment content={comment} />
+          return (
+            <Comment
+             key={comment}
+             content={comment} 
+             onDeleteComment={deleteComment} 
+            />
+          )
         })}
       </div>
     </article>
